cd99e54abac6651567a6b14bea3febae1e9c8ed8
loismichelant <160537903+loismichelant@users.noreply.github.com>
2025-04-14 18:11:06 -0400

    Add comments
    Commentaires -> Devise.java

---
+++ Devise.java
@@ -0,0 +1,51 @@
+package com.Bus.Model.Compte;
+
+/*
+ * Enumération représentant différentes devises monétaires.
+ * Chaque devise est associée à un symbole et un taux de conversion.
+ */
+
+public enum  Devise{
+	
+	// Dollar américain avec son symbole et son taux de conversion
+	USD( "$", 0.69),
+	
+	//  Euro avec son symbole et son taux de conversion
+	EUR( "€", 0.64),
+	
+	// Livre sterling avec son symbole et son taux de conversion
+	GBP( "£", 0.54);
+	
+	// Symbole de la devise
+	public String signe;
+	
+	// Taux de conversion de la devise par rapport à la devise de référence
+	public double exchangeRate;
+	
+	 /**
+	  * Constructeur de l'énumération Devise
+	  * 
+	  * @param signe
+	  * @param exchangeRate
+	  */
+	Devise(String signe, double exchangeRate) {
+		this.signe = signe;
+        this.exchangeRate = exchangeRate;
+	}
+	
+	/**
+	 * 
+	 * @return le symbole monétaire de la devise
+	 */
+	public String getSigne() {
+        return signe;
+    }
+	
+	/**
+	 * 
+	 * @return le taux de conversion de la devise vers la devise de référence
+	 */
+	public double getExchangeRate() {
+        return exchangeRate;
+    }
+}
