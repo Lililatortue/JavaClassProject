72ae8cea0e61a6aff5107d1038f14b3483f92d33
loismichelant <160537903+loismichelant@users.noreply.github.com>
2025-04-14 16:43:44 -0400

    Add files via upload
    Commentaires -> IInterestEvent.java

---
+++ IInterestEvent.java
@@ -0,0 +1,14 @@
+package com.Bus.Event;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/*
+ * Interface représentant un événement déclencheur lié aux intérêts bancaires
+ */
+public interface IInterestEvent {
+	/*
+     * Méthode appelée lorsqu'un événement d'intérêt survient
+     */
+	public void update();
+}
