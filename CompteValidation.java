5299cda7198eca8a20b903c71dcb3986836044d5
loismichelant <160537903+loismichelant@users.noreply.github.com>
2025-04-14 23:43:23 -0400

    Add comments
    Commentaires -> CompteValidation.java

---
+++ CompteValidation.java
@@ -0,0 +1,11 @@
+package com.Bus.Service.CompteManagement;
+
+/*
+ * Classe utilitaire pour la validation des données liées aux comptes bancaires.
+ * 
+ * Note: Actuellement vide, cette classe est prévue pour des extensions futures.
+ */
+
+public class CompteValidation {
+
+}
