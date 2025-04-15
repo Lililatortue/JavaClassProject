14ce7258419f34b161405bb46d4e5dd491daa98a
loismichelant <160537903+loismichelant@users.noreply.github.com>
2025-04-15 14:57:52 -0400

    Add comments
    Commentaires -> LoggerDatabase.java

---
+++ LoggerDatabase.java
@@ -0,0 +1,33 @@
+package com.Bus.Service.Logger;
+
+/*
+ * Implémentation de l'interface LoggerStrategy pour journaliser les messages dans une base de données.
+ * 
+ * !!! Actuellement, l'implémentation est incomplète et ne stocke pas encore les logs. !!!
+ */
+public class LoggerDatabase implements LoggerStrategy {
+	
+	// Chaîne de connexion à la base de données
+	private String connString;
+	
+	/**
+	 * Constructeur de la classe LoggerDatabase
+	 * 
+	 * @param connectionString - La chaîne de connexion à utiliser pour accéder à la base de données
+	 */
+	public LoggerDatabase(String connectionString) {
+		this.connString = connectionString;
+	}
+	
+	/**
+     * Méthode appelée pour journaliser un événement
+     * 
+     * @param logger - L'objet contenant les informations à journaliser
+     */
+	@Override
+	public void log(Logger logger) {
+			logger.toString(); // Implémentation incomplète
+	}
+	
+	
+}
