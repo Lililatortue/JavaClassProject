59b765fa6a6c2887414bf83c83cea5baba450d14
loismichelant <160537903+loismichelant@users.noreply.github.com>
2025-04-15 22:42:04 -0400

    Add comments
    Commentaires -> UserValidationException.java

---
+++ UserValidationException.java
@@ -0,0 +1,16 @@
+package com.Bus.Service.UserManagement;
+
+/*
+ * Exception personnalisée utilisée pour signaler des erreurs de validation liées aux utilisateurs.
+ */
+public class UserValidationException extends Exception {
+
+	/**
+	 * Construit une nouvelle exception avec le message d'erreur spécifié
+	 * 
+	 * @param message - Le message décrivant la cause de l'exception
+	 */
+	public UserValidationException(String message) {
+        super(message);
+    }
+}
